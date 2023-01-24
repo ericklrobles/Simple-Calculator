fe2207104a76077133d688e2380b92c7ff256ab4
ericklrobles <eglrobles@gmail.com>
2023-01-23 20:02:44 -0800

    commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Simple-Calculator
 Just a simple calculator I'm making for the first time.
+A program that simulates a simple calculator that can perform basic mathematical operations (e.g. addition, subtraction, multiplication, and division).
--- calculator.c
+++ calculator.c
@@ -6,6 +6,20 @@
 
 int main(void)
 {
- printf("calculator coming soonTM");
+ float num1, num2, result;
+ char operator;
+
+ printf("Enter the operator you would like to use today (+,-,x,/)\n");
+ scanf(" %c", &operator);
+
+ printf("Enter two numbers you would like to use with the operator that you chose\n");
+ scanf("%f", &num1);
+ scanf("%f", &num2);
+ 
+
+ if (operator == '+'){
+  result = (num1 + num2);
+  printf("%.2f", result);
+ }
  return 0;
 }
